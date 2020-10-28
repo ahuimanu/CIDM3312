ba9cfa7fa048aa399a44d1152e4d2b37b9bfbf1a
Jeffry Babb <ahuimanu@gmail.com>
2020-10-28 00:09:20 -0500

    updates to db and hw

--- VATSIMData/hw/Program.cs
+++ VATSIMData/hw/Program.cs
@@ -16,17 +16,29 @@ namespace hw
 
             using(var db = new VatsimDbContext())
             {
+
+                var _pilots = db.Pilots.Select(p => p).ToList();
+                Console.WriteLine($"The number of pilots records is: {_pilots.Count} ");
+
+                //1238470                
                 //UAL2865
-                //1238470
                 //20201013162413
-                var stuff = db.Pilots.Select(p => p).ToList();
-                Console.WriteLine($"The number of pilots records is: {stuff.Count} ");
-                var _pilot = db.Pilots.Find("1238470", "UAL2865", "20201013162413");
+                var _pilot = db.Pilots.Find("1238470", "UAL2865", "20201013162413");                
                 if(_pilot != null){
                     Console.WriteLine($"Pilot found: {_pilot.Realname}");
                 } else {
                     Console.WriteLine("Pilot not found");
-                }
+                }                
+
+                //1385451
+                //N130JM
+                //20201021233811
+                _pilot = db.Pilots.Find("1385451", "N130JM", "20201021233811");
+                if(_pilot != null){
+                    Console.WriteLine($"Pilot found: {_pilot.Realname}");
+                } else {
+                    Console.WriteLine("Pilot not found");
+                }                                
             }            
         }
     }
